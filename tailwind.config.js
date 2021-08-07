234e3651b721d93489dd71b3082e70830ef0def1
cris.dux <crisfer.4217@gmail.com>
2021-08-06 20:19:20 -0400

    primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      'body':['Inconsolata']
+    },
+    container:{
+      center:true,
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
